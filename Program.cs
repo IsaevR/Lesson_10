e692e346e6104c007ae51ac0d4558b01b110050b
IsaevR <diplomatic.area@gmail.com>
2018-05-16 08:53:09 +0300

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿
+/* Тестовое задание:  Написать метод Join который принимает массив строк, а возвращает
+ строку свормированную по следующему алгоритму:
+ 1.[] => ""
+ 2.[] => "1"and"2"
+ 3.[] => "1","2","3"and"4" .........................................................*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = new[] { "" };
+            string[] str1 = new[] { "1" };
+            string[] str2 = new[] { "1", "2" };
+            string[] str3 = new[] { "1", "2", "3", "4" , "5"};
+
+            string[] s = new string[str.Length];
+
+            s = Join(str);
+            foreach (string word in s)
+                Console.Write(word);
+            Console.WriteLine();
+
+            s = Join(str1);
+            foreach (string word in s)
+                Console.Write(word);
+            Console.WriteLine();
+
+            s = Join(str2);
+            foreach (string word in s)
+                Console.Write(word);
+            Console.WriteLine();
+
+            s = Join(str3);
+            foreach (string word in s)
+                Console.Write(word);
+            Console.WriteLine("\n\n\n\n\n");
+
+
+
+
+            List<string> w = new List<string>();
+            
+            w = Join1(str);
+            foreach (string q in w)   
+                Console.Write("{0}", q);
+            Console.WriteLine();
+
+            w = Join1(str1);
+            foreach (string q in w)
+                Console.Write("{0}", q);
+            Console.WriteLine();
+
+            w = Join1(str2);
+            foreach (string q in w)
+                Console.Write("{0}", q);
+            Console.WriteLine();
+
+            w = Join1(str3);
+            foreach (string q in w)
+                Console.Write("{0}", q);
+            Console.WriteLine();
+        }
+
+        // первый способ с примменением массивов 
+        static string[] Join (string[]str)
+        {
+            string comma = ", ";
+            string and = " and ";
+           
+            string[] line = new string[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i == str.Length - 2)                  
+                    str[i] += and;
+                    
+                else if(i != str.Length - 1)
+                    str[i] += comma; 
+
+                line[i] = str[i];   
+            }
+
+            return line;
+        }
+
+        // второй способ с применением коллекций
+        
+        static List<string> Join1(string[] s)
+        {
+            s.ToList();
+            s.Append(" and ");
+
+            return s.ToList();
+        }
+
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,48 @@
+﻿// Тестовое задание 2: Написать метод который переворачивает слова в тексте, но не меняет знаки
+// препинания и другие буквенные последовательности, например:
+// “text_one_123_two” => “txet_eno_123_owt
+using System;
+
+namespace TestDev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "text_one_123_two";
+
+            Console.Write("Исходная строка - {0}\n", line);
+            Console.Write("Перевернутая строка - ");
+            RevString(line);
+
+        }
+
+        static void RevString(string s)
+        {
+           
+            char[] arr = s.ToCharArray();
+            
+            s = "";
+            string c = "";
+
+            for (int i = 0; i < arr.Length ; i++)
+            {
+                if (arr[i] == 95 || (arr[i] > 47 || arr[i] < 58))
+                {
+                    for (int j = s.Length- 1; j == 0; j--)
+                    {
+                       c += s[j];
+                    }
+               
+                s = "";
+
+                }
+                s += arr[i];
+                
+            }
+           
+            Console.WriteLine(c);
+            
+        }
+    }
+}
